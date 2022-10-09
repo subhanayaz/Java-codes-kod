4cc966239442e5145577f499d54bb51e4de1f71e
Mohammed Subhan Ayaz Zama <66430769+subhanayaz@users.noreply.github.com>
2022-10-09 16:59:05 +0530

    Add files via upload

---
+++ Global Variable/src/variableType.java
@@ -0,0 +1,12 @@
+class variableType {
+	int value = 9;
+	String name = "Ayaz";
+	public static void main(String[] args) {
+		int p = 2;
+		String str = "Subhan";
+		System.out.println(value);
+		System.out.println(name);
+		System.out.println(p);
+		System.out.println(str);
+	}
+}
